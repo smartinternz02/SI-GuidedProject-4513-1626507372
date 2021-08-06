14d17250d2d4ecb4c37ebf2fca00e93cbabc0014
kyash21 <78461404+kyash21@users.noreply.github.com>
2021-08-06 14:12:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import numpy as np
+import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
