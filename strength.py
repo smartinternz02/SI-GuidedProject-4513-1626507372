0d47561495c088abaa14a1dc21644575f5938d3f
kyash21 <78461404+kyash21@users.noreply.github.com>
2021-08-06 14:13:37 +0530

    Update strength.py

--- strength.py
+++ strength.py
@@ -1,6 +1,4 @@
-Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> #import required libraries
+#import required libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
